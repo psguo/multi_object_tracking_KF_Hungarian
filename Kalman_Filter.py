95a620b0cd8e1fb6e6127f6514f414cf146bb899
psguo <guopengsheng@gmail.com>
2018-01-21 02:33:15 -0500

    kalman filter

---
+++ Kalman_Filter.py
@@ -0,0 +1,41 @@
+import numpy as np
+
+class Kalman_Filter(object):
+    def __init__(self, dt=0.1):
+        self.dt = dt
+
+        # dynamics
+        self.F = np.array([[1.0, self.dt], [0.0, 1.0]])
+        self.x = np.zeros((2, 1))
+        self.G = np.array([[self.dt**2/2], [self.dt]])
+        self.P = np.diag((3.0, 3.0))
+        self.Q = np.eye(self.x.shape[0])
+
+        # observation
+        self.y = np.array([[0], [255]])
+        self.H = np.array([[1, 0], [0, 1]])
+        self.R = np.eye(self.y.shape[0])
+
+        self.prevResult = np.array([[0], [255]])
+
+    def predict(self, u=0):
+        self.x = np.round(np.dot(self.F, self.x) + self.G * u)
+        self.P = np.dot(self.F, np.dot(self.P, self.F.T)) + self.Q
+        self.prevResult = self.x
+        return self.x
+
+    def correct(self, y, isDetected):
+        if not isDetected:
+            self.y = self.prevResult
+        else:
+            self.y = y
+        S = np.dot(self.H, np.dot(self.P, self.H.T)) + self.R
+        W = np.dot(self.P, np.dot(self.H.T, np.linalg.inv(S)))
+
+        return self.update(S, W)
+
+    def update(self, S, W):
+        self.x = np.round(self.x + np.dot(W, (self.y - np.dot(self.H, self.x))))
+        self.P = self.P - np.dot(W, np.dot(S, W.T))
+        self.prevResult = self.x
+        return self.x
